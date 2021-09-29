056791bc10820a711d9b9a186436f6f1d279cd3e
s-nes347 <kokuanes@yahoo.co.jp>
2021-09-29 10:08:03 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def puvlish(self):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
 
